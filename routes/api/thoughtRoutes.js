de5fd83ea2affe9ec1bd61d8787c58e38910bfca
chanjeff520 <chanjeff520@gmail.com>
2022-11-27 13:38:26 -0500

    add thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router;
+const {
+     getThoughts, 
+     getSingleThought, 
+     createThought, 
+     updateThought, 
+     deleteThought, 
+     addReaction,
+     removeReaction
+    } = require('../../controllers/userController');
+
+//get all thought or create new thought
+router.route('/').get(getThoughts).post(createThought);
+
+//get a single thought, delete a thought, or update a thought
+router.route('/:thought_id').get(getSingleThought).delete(deleteThought).put(updateThought);
+
+/* ------------------------ Reaction Routes -------------------------------- */
+
+//add or remove a reaction from the reactionlist in the thought model
+router.route('/:thought_id/reactions/:reaction_id').post(addReaction).delete(removeReaction);
+
+module.exports = router
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,23 @@
 const router = require('express').Router;
+const {
+     getUsers, 
+     createUser, 
+     getSingleUser, 
+     deleteUser, 
+     updateUser, 
+     addFriend,
+     removeFriend
+    } = require('../../controllers/userController');
 
 //get all user or create new user
 router.route('/').get(getUsers).post(createUser);
 
 //get a single user, delete a user, or update a user
-router.route('/:user_id').get(getSingleUser).delete(deleteUser).put("updateUser");
+router.route('/:user_id').get(getSingleUser).delete(deleteUser).put(updateUser);
 
+/* ------------------------ Friend Routes -------------------------------- */
+
+//add or remove a friend from the friendlist in the user model
+router.route('/:user_id/friends/:friend_id').post(addFriend).delete(removeFriend);
+
+module.exports = router
